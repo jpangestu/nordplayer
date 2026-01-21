dde77a024d3f7f10810226cb606d45ae787424e6
jpangestu <jpangestu111@gmail.com>
2026-01-21 18:55:20 +0700

    Force app rename

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -45,11 +45,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "nordplayer");
+    gtk_header_bar_set_title(header_bar, "suara");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "nordplayer");
+    gtk_window_set_title(window, "suara");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
