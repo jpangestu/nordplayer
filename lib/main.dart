0fb56eeb5943bbf67b727d035df404c9849f6772
jpangestu <jpangestu111@gmail.com>
2026-02-06 02:36:31 +0700

    Add window manager for handling window size

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:nordplayer/theme/nord_theme.dart';
 import 'package:nordplayer/pages/main_page.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await windowManager.ensureInitialized();
+
+  WindowOptions windowOptions = const WindowOptions(
+    size: Size(800, 600),
+    center: true,
+    backgroundColor: Colors.transparent,
+    skipTaskbar: false,
+    titleBarStyle: .normal,
+    windowButtonVisibility: true,
+  );
+
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
+
   runApp(const NordplayerApp());
 }
 
