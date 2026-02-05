bdf601c254a28c1a1c07dd254a104d34792b1cc0
jpangestu <jpangestu111@gmail.com>
2026-02-05 17:54:04 +0700

    Project rename

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const NordplayerApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class NordplayerApp extends StatelessWidget {
+  const NordplayerApp({super.key});
 
   // This widget is the root of your application.
   @override
